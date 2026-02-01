6233f80c4181f8854e928bd03c7f6c1492218da7
fvbanuel <72229317+fvbanuel@users.noreply.github.com>
2026-02-01 03:25:07 +0000

    Deploying to gh-pages from @ fvbanuel/fvbanuel.github.io@e6895e9665fbba8f805ed0f8ebb67279917a6d47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,11 +113,6 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-cv',
         title: 'CV',
